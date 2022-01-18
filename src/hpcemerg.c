a83449d6e5f66ff62f50c7c4c807a0a87ba20812
Ammar Faizi <ammarfaizi2@gmail.com>
2022-01-18 08:57:21 +0700

    hpcemerg: Prepare basic functionalities
    This adds init functions, context struct, param and several basic
    functionalities to start working with the project. We won't run
    out of things to work on anytime soon!
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/hpcemerg.c
+++ src/hpcemerg.c
@@ -5,5 +5,80 @@
 
 #include "hpcemerg.h"
 
+#include <errno.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/mman.h>
+
+static void internal_hpcemerg_handler(int sig, siginfo_t *si, void *arg)
+{
+	(void) sig;
+	(void) si;
+	(void) arg;
+}
+
+static int install_signal_handler(struct hpcemerg_ctx *ctx)
+{
+	struct sigaction act;
+
+	memset(&act, 0, sizeof(act));
+	act.sa_sigaction = &internal_hpcemerg_handler;
+	act.sa_flags = SA_SIGINFO | SA_ONSTACK;
+
+	if (ctx->param.handle_bits & HPCEMERG_HANDLE_ALL) {
+		stack_t st;
+		st.ss_sp = ctx->stack;
+		st.ss_flags = 0;
+		st.ss_size = sizeof(ctx->stack);
+		if (sigaltstack(&st, NULL))
+			return -errno;
+	}
 
 
+	return 0;
+}
+
+static int _hpcemerg_init(struct hpcemerg_ctx *ctx)
+{
+	int ret;
+	struct hpcemerg_init_param *p = &ctx->param;
+
+	if (p->handle_bits & ~HPCEMERG_HANDLE_ALL)
+		return -EINVAL;
+
+	ret = install_signal_handler(ctx);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+int hpcemerg_init(struct hpcemerg_init_param *p, struct hpcemerg_ctx **ctx_p)
+{
+	int ret;
+	struct hpcemerg_ctx *ctx;
+
+	ctx = mmap(NULL, sizeof(*ctx), PROT_READ | PROT_EXEC,
+		   MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (ctx == MAP_FAILED)
+		return -errno;
+
+	if (mlock(ctx, sizeof(*ctx)) < 0) {
+		ret = -errno;
+		goto out_unmap;
+	}
+
+	ctx->param = *p;
+	ret = _hpcemerg_init(ctx);
+	if (ret < 0)
+		goto out_unlock;
+
+	*ctx_p = ctx;
+	return 0;
+
+out_unlock:
+	munlock(ctx, sizeof(*ctx));
+out_unmap:
+	munmap(ctx, sizeof(*ctx));
+	return ret;
+}
--- src/hpcemerg.h
+++ src/hpcemerg.h
@@ -6,8 +6,55 @@
 #ifndef LIBHPCEMERG_H__
 #define LIBHPCEMERG_H__
 
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+
+#include <signal.h>
+#include <stdint.h>
+#include <ucontext.h>
+#include <execinfo.h>
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#define HPCEMERG_INTR_STKSZ (1024 * 1024)
+
+#include <dlfcn.h>
+
+struct hpcemerg_init_param {
+	void		(*user_func)(int sig, siginfo_t *si, ucontext_t *ctx);
+	uint32_t	handle_bits;
+	uint32_t	resv[3];
+	uint8_t		nr_thpool;
+	uint8_t		resv2[7];
+};
+
+struct hpcemerg_ctx {
+	uint8_t				stack[HPCEMERG_INTR_STKSZ];
+	struct hpcemerg_init_param	param;
+};
+
 extern void hpcemerg_fentry_enable(void);
 extern void hpcemerg_fentry_disable(void);
 extern void hpcemerg_fentry_set_target_func(void (*target)(void));
+
+extern int hpcemerg_init(struct hpcemerg_init_param *p,
+			 struct hpcemerg_ctx **ctx_p);
+extern void hpcemerg_destroy(struct hpcemerg_ctx *p);
+
+#define HPCEMERG_HANDLE_BUG	(1u << 0u)
+#define HPCEMERG_HANDLE_WARN	(1u << 1u)
+#define HPCEMERG_HANDLE_SIGSEGV	(1u << 2u)
+#define HPCEMERG_HANDLE_SIGILL	(1u << 3u)
+#define HPCEMERG_HANDLE_SIGFPE	(1u << 4u)
+#define HPCEMERG_HANDLE_ALL (		\
+	HPCEMERG_HANDLE_BUG	|	\
+	HPCEMERG_HANDLE_WARN	|	\
+	HPCEMERG_HANDLE_SIGSEGV	|	\
+	HPCEMERG_HANDLE_SIGILL	|	\
+	HPCEMERG_HANDLE_SIGILL		\
+)
 
 #endif /* #ifndef LIBHPCEMERG_H__ */
